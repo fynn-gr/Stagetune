64a60776a5bc412470b7f70f1cfcf7494e92db4f
TheFynn <fynn-2002@web.de>
2022-03-05 15:45:19 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <playlist-item/>
+</template>
+
+<script>
+import playlistItem from './components/playlistItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    playlistItem,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/playlistItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="playlistItem">
+    <h1>Title</h1>
+    <h2>path of the Track</h2>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  name: 'playlistItem',
+  props: {
+    name: String
+  }
+}
+</script>
+
+
+
+
+<style scoped lang="scss">
+
+.playlistItem {
+  
+}
+
+</style>
