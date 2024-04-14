e9ebb4f36e549ebec8b62856205b2438c395d21b
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-04-14 18:06:05 +0200

    fix annotation html save issue

--- src/lib/Annotation.svelte
+++ src/lib/Annotation.svelte
@@ -111,10 +111,13 @@
 			}}
 			on:blur={() => {
 				isEditing.update(e => e - 1);
+				console.log("old text: ", annotation.text);
+				console.log("inner HTML: ", annotationEl.innerHTML)
 				annotation.text = annotationEl.innerHTML;
+				console.log("new save: ", annotation.text)
 			}}
 		>
-			<p>{@html annotation.text}</p>
+			{@html annotation.text}
 		</div>
 	</div>
 {:else}
